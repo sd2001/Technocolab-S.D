1b87cff463871e995bd4eb2cdb3ef5eecc300c21
sd2001 <im.swarnabha2001@gmail.com>
2020-07-02 21:11:39 +0530

    Sunset Commit

---
+++ Weather Data/sunset_rise.py
@@ -0,0 +1,12 @@
+import pyowm
+
+owm=pyowm.OWM('0833f103dc7c2924da06db624f74565c')
+mgr=owm.weather_manager()
+place=(input("Enter the city name : "))
+obs=mgr.weather_at_place(place)
+weather=obs.weather
+print(f"Sunset time in {place} is ",weather.sunset_time(timeformat='iso'))
+print(f"Sunrise time in {place} is ",weather.sunrise_time(timeBoston, USformat='iso'))
+
+#input-Enter the city name : Boston, US
+#output-Sunset time in Boston, US is  2020-07-03 00:24:34+00     Sunrise time in Boston, US is  2020-07-02 09:11:52+00
