bd194eec8acc3c395fbcf149546d73cb698f20e4
sd2001 <im.swarnabha2001@gmail.com>
2020-07-03 10:27:55 +0530

    Final Commits

--- Weather Data/sunset_rise.py
+++ Weather Data/sunset_rise.py
@@ -6,7 +6,7 @@ place=(input("Enter the city name : "))
 obs=mgr.weather_at_place(place)
 weather=obs.weather
 print(f"Sunset time in {place} is ",weather.sunset_time(timeformat='iso'))
-print(f"Sunrise time in {place} is ",weather.sunrise_time(timeBoston, USformat='iso'))
+print(f"Sunrise time in {place} is ",weather.sunrise_time(timeformat='iso'))
 
 #input-Enter the city name : Boston, US
 #output-Sunset time in Boston, US is  2020-07-03 00:24:34+00     Sunrise time in Boston, US is  2020-07-02 09:11:52+00
