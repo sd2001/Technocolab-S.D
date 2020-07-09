284fdc993b3bdb72e3c660be65e9dd54eb7c3d23
sd2001 <im.swarnabha2001@gmail.com>
2020-07-09 18:17:24 +0530

    Line_graph changes

--- Final Project/line_forecast.py
+++ Final Project/line_forecast.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib import dates
 from datetime import datetime
 import pyowm
+from matplotlib import rcParams
 
 owm=pyowm.OWM('0833f103dc7c2924da06db624f74565c')
 mgr=owm.weather_manager()
@@ -39,31 +40,21 @@ def find_min_max(place,unit):
     plot_bars(days,min_t,max_t)
     #return days,min_t,max_t
 
-def plot_bars(days,min_t,max_t):  
-        print(days)      
+def plot_bars(days,min_t,max_t):        
         days=dates.date2num(days)
-        print(days) 
-        min_temp_bar=plt.bar(days-0.2, min_t, width=0.4, color='r')
-        max_temp_bar=plt.bar(days+0.2, max_t, width=0.4, color='b')        
+        #rcParams['figure.figsize']=10,7
+        plt.plot(days,max_t,color='green', linestyle='dashdot', linewidth = 1,marker='o', markerfacecolor='red', markersize=7) 
+        plt.plot(days,min_t,color='orange', linestyle='dashdot', linewidth = 1,marker='o', markerfacecolor='blue', markersize=7)     
+        #plt.ylim(min(min_t)-10,max(max_t)+10)
         plt.xticks(days)
         x_y_axis=plt.gca()
         xaxis_format=dates.DateFormatter('%m/%d')
         
         x_y_axis.xaxis.set_major_formatter(xaxis_format)
-        
-        for bar_chart in [min_temp_bar,max_temp_bar]:
-            for index,bar in enumerate(bar_chart):
-                height=bar.get_height()
-                xpos=bar.get_x()+bar.get_width()/2.0
-                ypos=height 
-                label_text=str(int(height))
-                plt.text(xpos, ypos,label_text,
-                        horizontalalignment='center',
-                        verticalalignment='bottom',
-                        color='black')
-        
-        
-        plt.savefig('figure.png')
+        plt.grid(True,color='brown')
+        plt.legend(["Max","Min"],loc=2)    
+        plt.show()
+        plt.savefig('figure_line.png')
     
     
 find_min_max('Kolkata','Celcius')
